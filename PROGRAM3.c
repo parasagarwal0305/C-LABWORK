d76fc79e9f193e1292d7c93570899c17397acb63
parasagarwal0305 <parasagarwal0305@gmail.com>
2025-09-01 16:44:00 +0530

    initial commit

---
+++ PROGRAM3.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+int main()
+{
+    int i,count, sum=0;
+    for(i=10;i<=70;i++)
+    {
+     if(i%2!=0)
+     {
+     count+=1;
+     
+        if(count<=12)
+        {
+        sum+=i;
+    }
+}
+
+
+}
+printf("%d",sum);
+}
